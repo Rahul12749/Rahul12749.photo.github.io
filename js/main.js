9e3045e40764a8b877d1dc7adc0bbe1a5f8e7bcd
Rahul12749 <74234883+Rahul12749@users.noreply.github.com>
2023-04-01 20:01:30 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/**
+* Template Name: PhotoFolio - v1.1.0
+* Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  }
+
+  
+});
